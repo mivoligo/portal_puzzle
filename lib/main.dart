e361c69298a8f6d2d792077050f8db676cbd288f
Michal Predotka <mivoligo@gmail.com>
2022-02-08 21:12:45 +0000

    Add moving rows and columns box

--- lib/main.dart
+++ lib/main.dart
@@ -31,19 +31,101 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final boxes = _generateGameBoxes();
+  GameBox? tappedBox;
+  late Offset tappedLoc;
+  late List<GameBox> tappedRow;
+  late List<GameBox> tappedColumn;
+
+  GameBox? _getTappedBox(Offset globalCoords) {
+    for (GameBox box in boxes) {
+      if (box.getRect(MediaQuery.of(context).size).contains(globalCoords)) {
+        return box;
+      }
+    }
+    return null;
+  }
+
+  List<GameBox> _getRowMatesForBox(GameBox box) {
+    final rowMates = <GameBox>[];
+    for (GameBox rowMateCandidateBox in boxes) {
+      if (box.loc.dy == rowMateCandidateBox.loc.dy) {
+        rowMates.add(rowMateCandidateBox);
+      }
+    }
+    return rowMates;
+  }
+
+  List<GameBox> _getColumnMatesForBox(GameBox box) {
+    final columnMates = <GameBox>[];
+    for (GameBox columnMateCandidateBox in boxes) {
+      if (box.loc.dx == columnMateCandidateBox.loc.dx) {
+        columnMates.add(columnMateCandidateBox);
+      }
+    }
+    return columnMates;
+  }
+
+  void _snapBoxes() {
+    for (GameBox box in boxes) {
+      Offset translatedLoc = box.loc + Offset(0.5, 0.5);
+      box.loc = Offset(
+          translatedLoc.dx.round() - 0.5, translatedLoc.dy.round() - 0.5);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    Rect gameBoxRect = GameBox(
+      startLoc: Offset.zero,
+      loc: Offset.zero,
+      color: Colors.black,
+    ).getRect(screenSize);
+
     return Scaffold(
       body: Center(
         child: GestureDetector(
+          onPanStart: (details) {
+            tappedBox = _getTappedBox(details.globalPosition);
+            tappedLoc = details.globalPosition;
+            tappedRow = _getRowMatesForBox(tappedBox!);
+            tappedColumn = _getColumnMatesForBox(tappedBox!);
+          },
+          onPanUpdate: (details) {
+            Offset dragOffset = details.globalPosition - tappedLoc;
+            setState(() {
+              if (tappedBox != null) {
+                if (dragOffset.dx.abs() > dragOffset.dy.abs()) {
+                  for (GameBox box in tappedRow) {
+                    box.loc = box.startLoc +
+                        Offset(dragOffset.dx / gameBoxRect.width, 0);
+                  }
+                } else {
+                  for (GameBox box in tappedColumn) {
+                    box.loc = box.startLoc +
+                        Offset(0, dragOffset.dy / gameBoxRect.height);
+                  }
+                }
+              }
+            });
+          },
+          onPanEnd: (detail) {
+            setState(() {
+              _snapBoxes();
+              for (GameBox box in boxes) {
+                box.startLoc = box.loc;
+              }
+            });
+          },
           child: Stack(
             alignment: Alignment.center,
             children: boxes
-                .map((e) => GameBoxWidget(
-                      box: e,
-                      text: boxes.indexOf(e).toString(),
-                    ))
+                .map(
+                  (e) => GameBoxWidget(
+                    box: e,
+                    text: boxes.indexOf(e).toString(),
+                  ),
+                )
                 .toList(),
           ),
         ),
@@ -54,12 +136,13 @@ class _HomePageState extends State<HomePage> {
 
 class GameBox {
   GameBox({
+    required this.startLoc,
     required this.loc,
     required this.color,
   });
 
-  final Offset loc;
-
+  Offset startLoc;
+  Offset loc;
   final Color color;
 
   Rect getRect(Size parentSize) {
@@ -108,9 +191,10 @@ class GameBoxWidget extends StatelessWidget {
 
 List<GameBox> _generateGameBoxes() {
   final result = <GameBox>[];
-  for (double x = -2.5; x <= 2.5; x++) {
-    for (double y = -2.5; y <= 2.5; y++) {
-      result.add(GameBox(loc: Offset(x, y), color: Colors.red));
+  for (double y = -1; y <= 1; y++) {
+    for (double x = -1; x <= 1; x++) {
+      result.add(GameBox(
+          startLoc: Offset(x, y), loc: Offset(x, y), color: Colors.red));
     }
   }
   return result;
