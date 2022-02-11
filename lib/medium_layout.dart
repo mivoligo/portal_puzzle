e188731dde1b7c2a86b870b0ac9d417a37676395
Michal Predotka <mivoligo@gmail.com>
2022-02-11 20:49:00 +0000

    Improve medium layout UI

--- lib/medium_layout.dart
+++ lib/medium_layout.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:portal_puzzle/app_title.dart';
 
+import 'difficulty_selector.dart';
 import 'game_board.dart';
+import 'hero_button.dart';
 
 class MediumLayout extends StatelessWidget {
   const MediumLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const AppTitle(),
-        Expanded(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return GameBoard(parentSize: constraints.biggest);
-            },
+    return LayoutBuilder(builder: (context, constraints) {
+      return Stack(
+        alignment: Alignment.center,
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      AppTitle(),
+                      DifficultySelector(),
+                    ],
+                  ),
+                ),
+                const Text(
+                  '133 moves',
+                  style: TextStyle(fontSize: 24),
+                ),
+                const SizedBox(height: 12),
+                GameBoard(parentSize: constraints.biggest * 0.6),
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: HeroButton(
+                    label: 'Shuffle',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    });
   }
 }
