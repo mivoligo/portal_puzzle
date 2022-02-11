f0368b96b0d320b39af7b212fe7c7fcfe13344e6
Michal Predotka <mivoligo@gmail.com>
2022-02-11 20:35:03 +0000

    Improve small layout UI

--- lib/small_layout.dart
+++ lib/small_layout.dart
@@ -2,29 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:portal_puzzle/app_title.dart';
 import 'package:portal_puzzle/difficulty_selector.dart';
 import 'package:portal_puzzle/game_board.dart';
+import 'package:portal_puzzle/hero_button.dart';
 
 class SmallLayout extends StatelessWidget {
   const SmallLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const AppTitle(),
-        Expanded(
-          child: Column(
-            children: [
-              const DifficultySelector(),
-              const SizedBox(height: 12),
-              LayoutBuilder(
-                builder: (context, constraints) {
-                  return GameBoard(parentSize: constraints.biggest);
-                },
-              ),
-            ],
+    return LayoutBuilder(builder: (context, constraints) {
+      return Stack(
+        alignment: Alignment.center,
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(32.0),
+                  child: AppTitle(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: DifficultySelector(),
+                ),
+                const Text(
+                  '1337 moves',
+                  style: TextStyle(fontSize: 24),
+                ),
+                const SizedBox(height: 12),
+                GameBoard(parentSize: constraints.biggest * 0.8),
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: HeroButton(
+                    label: 'Shuffle',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    });
   }
 }
