d8ea7ba110c993350b62dca98925b889410407ac
Michal Predotka <mivoligo@gmail.com>
2022-02-12 20:54:43 +0000

    Add some style to difficulty selector

--- lib/widgets/difficulty_selector.dart
+++ lib/widgets/difficulty_selector.dart
@@ -16,18 +16,29 @@ class DifficultySelector extends StatelessWidget {
         _DifficultyButton(
           text: 'Simple',
           isSelected: difficulty == Difficulty.simple,
+          surfaceColor: const Color(0xFF22C55E),
+          sideColor: const Color(0xFF166534),
+          textColor: const Color(0xFF14532D),
           onPressed: () =>
               context.read<GameModel>().setDifficulty(Difficulty.simple),
         ),
+        const SizedBox(width: 12),
         _DifficultyButton(
           text: 'Medium',
           isSelected: difficulty == Difficulty.medium,
+          surfaceColor: const Color(0xFF3B82F6),
+          sideColor: const Color(0xFF1E40AF),
+          textColor: const Color(0xFF1E3A8A),
           onPressed: () =>
               context.read<GameModel>().setDifficulty(Difficulty.medium),
         ),
+        const SizedBox(width: 12),
         _DifficultyButton(
           text: 'Hard',
           isSelected: difficulty == Difficulty.hard,
+          surfaceColor: const Color(0xFF8B5CF6),
+          sideColor: const Color(0xFF5B21B6),
+          textColor: const Color(0xFF4C1D95),
           onPressed: () =>
               context.read<GameModel>().setDifficulty(Difficulty.hard),
         ),
@@ -36,28 +47,67 @@ class DifficultySelector extends StatelessWidget {
   }
 }
 
-class _DifficultyButton extends StatelessWidget {
+class _DifficultyButton extends StatefulWidget {
   const _DifficultyButton({
     Key? key,
     required this.isSelected,
     required this.text,
+    required this.surfaceColor,
+    required this.sideColor,
+    required this.textColor,
     required this.onPressed,
   }) : super(key: key);
 
   final bool isSelected;
   final String text;
+  final Color surfaceColor;
+  final Color sideColor;
+  final Color textColor;
   final VoidCallback onPressed;
 
   @override
+  State<_DifficultyButton> createState() => _DifficultyButtonState();
+}
+
+class _DifficultyButtonState extends State<_DifficultyButton> {
+  bool isPressed = false;
+
+  @override
   Widget build(BuildContext context) {
-    return TextButton(
-      child: Text(
-        text,
-        style: isSelected
-            ? const TextStyle(color: Colors.orange)
-            : const TextStyle(color: Colors.grey),
+    return GestureDetector(
+      onTapDown: (_) => widget.onPressed(),
+      child: Transform.translate(
+        offset: widget.isSelected ? const Offset(0, 4) : const Offset(0, 0),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          decoration: BoxDecoration(
+            color: widget.surfaceColor,
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            boxShadow: [
+              BoxShadow(
+                  color: const Color(0xEE222222),
+                  offset: widget.isSelected
+                      ? const Offset(0, 2)
+                      : const Offset(0, 6),
+                  blurRadius: widget.isSelected ? 2 : 8,
+                  blurStyle: BlurStyle.outer),
+              BoxShadow(
+                color: widget.sideColor,
+                offset:
+                    widget.isSelected ? const Offset(0, 2) : const Offset(0, 6),
+              ),
+            ],
+          ),
+          child: Text(
+            widget.text,
+            style: widget.isSelected
+                ? const TextStyle(
+                    color: Color(0xFFFFFFFF), fontWeight: FontWeight.w600)
+                : TextStyle(
+                    color: widget.textColor, fontWeight: FontWeight.w600),
+          ),
+        ),
       ),
-      onPressed: onPressed,
     );
   }
 }
--- lib/widgets/hero_button.dart
+++ lib/widgets/hero_button.dart
@@ -30,49 +30,44 @@ class _HeroButtonState extends State<HeroButton> {
       onTapCancel: () => setState(() {
         isPressed = false;
       }),
-      child: Column(
-        children: [
-          Transform.translate(
-            offset: isPressed ? const Offset(0, 6) : const Offset(0, 0),
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Icon(
-                    Icons.refresh,
-                    color: Color(0xFF7F1D1D),
-                  ),
-                  const SizedBox(width: 12),
-                  Text(
-                    widget.label,
-                    style: const TextStyle(
-                      color: Color(0xFF7F1D1D),
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ],
+      child: Transform.translate(
+        offset: isPressed ? const Offset(0, 6) : const Offset(0, 0),
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.refresh,
+                color: Color(0xFF7F1D1D),
               ),
-              decoration: BoxDecoration(
-                color: const Color(0xFFEF4444),
-                borderRadius: const BorderRadius.all(Radius.circular(12)),
-                boxShadow: [
-                  BoxShadow(
-                      color: const Color(0xEE222222),
-                      offset:
-                          isPressed ? const Offset(0, 2) : const Offset(0, 8),
-                      blurRadius: isPressed ? 2 : 12,
-                      blurStyle: BlurStyle.outer),
-                  BoxShadow(
-                    color: const Color(0xFF991B1B),
-                    offset: isPressed ? const Offset(0, 2) : const Offset(0, 8),
-                  ),
-                ],
+              const SizedBox(width: 12),
+              Text(
+                widget.label,
+                style: const TextStyle(
+                  color: Color(0xFF7F1D1D),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+          decoration: BoxDecoration(
+            color: const Color(0xFFEF4444),
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+            boxShadow: [
+              BoxShadow(
+                  color: const Color(0xEE222222),
+                  offset: isPressed ? const Offset(0, 2) : const Offset(0, 8),
+                  blurRadius: isPressed ? 2 : 12,
+                  blurStyle: BlurStyle.outer),
+              BoxShadow(
+                color: const Color(0xFF991B1B),
+                offset: isPressed ? const Offset(0, 2) : const Offset(0, 8),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
