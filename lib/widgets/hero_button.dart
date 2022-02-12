7f39a83f8a0787d6298a98e3c73c82dff62659d8
Michal Predotka <mivoligo@gmail.com>
2022-02-12 18:54:34 +0000

    Customize shuffle button

--- lib/widgets/hero_button.dart
+++ lib/widgets/hero_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HeroButton extends StatelessWidget {
+class HeroButton extends StatefulWidget {
   const HeroButton({
     Key? key,
     required this.label,
@@ -11,15 +11,69 @@ class HeroButton extends StatelessWidget {
   final VoidCallback? onPressed;
 
   @override
+  State<HeroButton> createState() => _HeroButtonState();
+}
+
+class _HeroButtonState extends State<HeroButton> {
+  bool isPressed = false;
+
+  @override
   Widget build(BuildContext context) {
-    return TextButton.icon(
-      icon: const Icon(Icons.refresh),
-      label:  Text(label),
-      style: TextButton.styleFrom(
-          backgroundColor: Colors.orange,
-          primary: Colors.white,
-          padding: const EdgeInsets.all(24)),
-      onPressed: onPressed,
+    return GestureDetector(
+      onTap: widget.onPressed,
+      onTapDown: (_) => setState(() {
+        isPressed = true;
+      }),
+      onTapUp: (_) => setState(() {
+        isPressed = false;
+      }),
+      onTapCancel: () => setState(() {
+        isPressed = false;
+      }),
+      child: Column(
+        children: [
+          Transform.translate(
+            offset: isPressed ? const Offset(0, 6) : const Offset(0, 0),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(
+                    Icons.refresh,
+                    color: Color(0xFF7F1D1D),
+                  ),
+                  const SizedBox(width: 12),
+                  Text(
+                    widget.label,
+                    style: const TextStyle(
+                      color: Color(0xFF7F1D1D),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: const Color(0xFFEF4444),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0xEE222222),
+                      offset:
+                          isPressed ? const Offset(0, 2) : const Offset(0, 8),
+                      blurRadius: isPressed ? 2 : 12,
+                      blurStyle: BlurStyle.outer),
+                  BoxShadow(
+                    color: const Color(0xFF991B1B),
+                    offset: isPressed ? const Offset(0, 2) : const Offset(0, 8),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
