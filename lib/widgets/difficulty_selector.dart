51ac62f05bc07054639b4518314e01168b682f75
Michal Predotka <mivoligo@gmail.com>
2022-02-13 16:30:11 +0000

    Refactor difficulty selector

--- lib/widgets/difficulty_selector.dart
+++ lib/widgets/difficulty_selector.dart
@@ -15,10 +15,10 @@ class DifficultySelector extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       children: [
         _DifficultyButton(
-          text: 'Simple',
+          text: 'Easy',
           isSelected: difficulty == Difficulty.simple,
           surfaceColor: const Color(0xFF22C55E),
-          sideColor: const Color(0xFF166534),
+          sideColor: const Color(0xFF059669),
           textColor: const Color(0xFF14532D),
           onPressed: () {
             context.read<GameModel>().setDifficulty(Difficulty.simple);
@@ -27,10 +27,10 @@ class DifficultySelector extends StatelessWidget {
         ),
         const SizedBox(width: 12),
         _DifficultyButton(
-          text: 'Medium',
+          text: 'Normal',
           isSelected: difficulty == Difficulty.medium,
           surfaceColor: const Color(0xFF3B82F6),
-          sideColor: const Color(0xFF1E40AF),
+          sideColor: const Color(0xFF1D4ED8),
           textColor: const Color(0xFF1E3A8A),
           onPressed: () {
             context.read<GameModel>().setDifficulty(Difficulty.medium);
@@ -42,7 +42,7 @@ class DifficultySelector extends StatelessWidget {
           text: 'Hard',
           isSelected: difficulty == Difficulty.hard,
           surfaceColor: const Color(0xFF8B5CF6),
-          sideColor: const Color(0xFF5B21B6),
+          sideColor: const Color(0xFF6D28D9),
           textColor: const Color(0xFF4C1D95),
           onPressed: () {
             context.read<GameModel>().setDifficulty(Difficulty.hard);
@@ -54,7 +54,7 @@ class DifficultySelector extends StatelessWidget {
   }
 }
 
-class _DifficultyButton extends StatefulWidget {
+class _DifficultyButton extends StatelessWidget {
   const _DifficultyButton({
     Key? key,
     required this.isSelected,
@@ -63,6 +63,7 @@ class _DifficultyButton extends StatefulWidget {
     required this.sideColor,
     required this.textColor,
     required this.onPressed,
+    this.isPressed = false,
   }) : super(key: key);
 
   final bool isSelected;
@@ -72,46 +73,37 @@ class _DifficultyButton extends StatefulWidget {
   final Color textColor;
   final VoidCallback onPressed;
 
-  @override
-  State<_DifficultyButton> createState() => _DifficultyButtonState();
-}
-
-class _DifficultyButtonState extends State<_DifficultyButton> {
-  bool isPressed = false;
+  final bool isPressed;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTapDown: (_) => widget.onPressed(),
+      onTapDown: (_) => onPressed(),
       child: Transform.translate(
-        offset: widget.isSelected ? const Offset(0, 4) : const Offset(0, 0),
+        offset: isSelected ? const Offset(0, 4) : const Offset(0, 0),
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
           decoration: BoxDecoration(
-            color: widget.surfaceColor,
+            color: surfaceColor,
             borderRadius: const BorderRadius.all(Radius.circular(8)),
             boxShadow: [
               BoxShadow(
                   color: const Color(0xEE222222),
-                  offset: widget.isSelected
-                      ? const Offset(0, 2)
-                      : const Offset(0, 6),
-                  blurRadius: widget.isSelected ? 2 : 8,
+                  offset: isSelected ? const Offset(0, 2) : const Offset(0, 6),
+                  blurRadius: isSelected ? 2 : 8,
                   blurStyle: BlurStyle.outer),
               BoxShadow(
-                color: widget.sideColor,
-                offset:
-                    widget.isSelected ? const Offset(0, 2) : const Offset(0, 6),
+                color: sideColor,
+                offset: isSelected ? const Offset(0, 2) : const Offset(0, 6),
               ),
             ],
           ),
           child: Text(
-            widget.text,
-            style: widget.isSelected
+            text,
+            style: isSelected
                 ? const TextStyle(
                     color: Color(0xFFFFFFFF), fontWeight: FontWeight.w600)
-                : TextStyle(
-                    color: widget.textColor, fontWeight: FontWeight.w600),
+                : TextStyle(color: textColor, fontWeight: FontWeight.w600),
           ),
         ),
       ),
