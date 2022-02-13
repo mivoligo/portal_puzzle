528201157db4b0b43af885e8528f19728694e37b
Michal Predotka <mivoligo@gmail.com>
2022-02-13 21:18:30 +0000

    Add tween animation to hero button

--- lib/widgets/hero_button.dart
+++ lib/widgets/hero_button.dart
@@ -19,6 +19,7 @@ class _HeroButtonState extends State<HeroButton> {
 
   @override
   Widget build(BuildContext context) {
+    double elevation = isPressed ? 0 : 6;
     return GestureDetector(
       onTap: widget.onPressed,
       onTapDown: (_) => setState(() {
@@ -30,43 +31,50 @@ class _HeroButtonState extends State<HeroButton> {
       onTapCancel: () => setState(() {
         isPressed = false;
       }),
-      child: Transform.translate(
-        offset: isPressed ? const Offset(0, 6) : const Offset(0, 0),
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Icon(
-                Icons.refresh,
+      child: TweenAnimationBuilder(
+        tween: Tween<double>(begin: elevation, end: elevation),
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeOut,
+        builder: (_, double value, child) {
+          return Transform.translate(
+            offset: Offset(0, -value),
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: child,
+              decoration: BoxDecoration(
+                color: const Color(0xFFEF4444),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+                boxShadow: [
+                  BoxShadow(
+                      color: const Color(0xEE222222),
+                      offset: Offset(0, value + 2),
+                      blurRadius: value + 2,
+                      blurStyle: BlurStyle.outer),
+                  BoxShadow(
+                      color: const Color(0xFF991B1B),
+                      offset: Offset(0, value + 2)),
+                ],
+              ),
+            ),
+          );
+        },
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(
+              Icons.refresh,
+              color: Color(0xFF7F1D1D),
+            ),
+            const SizedBox(width: 12),
+            Text(
+              widget.label,
+              style: const TextStyle(
                 color: Color(0xFF7F1D1D),
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
               ),
-              const SizedBox(width: 12),
-              Text(
-                widget.label,
-                style: const TextStyle(
-                  color: Color(0xFF7F1D1D),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
-          ),
-          decoration: BoxDecoration(
-            color: const Color(0xFFEF4444),
-            borderRadius: const BorderRadius.all(Radius.circular(12)),
-            boxShadow: [
-              BoxShadow(
-                  color: const Color(0xEE222222),
-                  offset: isPressed ? const Offset(0, 2) : const Offset(0, 8),
-                  blurRadius: isPressed ? 2 : 12,
-                  blurStyle: BlurStyle.outer),
-              BoxShadow(
-                color: const Color(0xFF991B1B),
-                offset: isPressed ? const Offset(0, 2) : const Offset(0, 8),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
