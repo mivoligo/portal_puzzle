d067bdf1059946c7f3368cc9e0a7fce4fdaa52bf
Michal Predotka <mivoligo@gmail.com>
2022-02-21 22:11:41 +0000

    Add opacity animation

--- lib/widgets/animated_tile.dart
+++ lib/widgets/animated_tile.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
 
-class AnimatedTile extends AnimatedWidget {
+class AnimatedTile extends StatelessWidget {
   AnimatedTile({
     Key? key,
     required this.child,
     required int index,
     required Animation<double> animation,
-  })  : scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+  })  : _scaleAnimation = Tween<double>(begin: 1.0, end: 4.0).animate(
           CurvedAnimation(
             parent: animation,
             curve: Interval(
               0.05 * index,
               0.2 + 0.05 * index,
-              curve: Curves.bounceInOut,
+              curve: Curves.easeIn,
             ),
           ),
         ),
-        super(key: key, listenable: animation);
+        _opacityAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
+          CurvedAnimation(
+            parent: animation,
+            curve: Interval(
+              0.05 * index,
+              0.2 + 0.05 * index,
+              curve: Curves.easeInOut,
+            ),
+          ),
+        ),
+        super(key: key);
 
   final Widget child;
-  final Animation<double> scaleAnimation;
+  final Animation<double> _scaleAnimation;
+  final Animation<double> _opacityAnimation;
 
   @override
   Widget build(BuildContext context) {
-    return Transform.scale(
-      scale: scaleAnimation.value * 2 + 1,
-      child: child,
+    return ScaleTransition(
+      scale: _scaleAnimation,
+      child: FadeTransition(
+        opacity: _opacityAnimation,
+        child: child,
+      ),
     );
   }
 }
