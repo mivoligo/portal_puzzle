27e3895935874581338d71c7db2a3f2443263786
Michal Predotka <mivoligo@gmail.com>
2022-02-21 14:45:45 +0000

    Improve web specific content

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Portal Puzzle',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Rubik',
