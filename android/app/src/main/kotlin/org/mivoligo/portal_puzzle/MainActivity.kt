edb6b6a85850a6537dba16f87aa6c383d0b9e508
Michal Predotka <mivoligo@gmail.com>
2022-03-23 21:42:43 +0000

    Add splash screen for Android

--- android/app/src/main/kotlin/org/mivoligo/portal_puzzle/MainActivity.kt
+++ android/app/src/main/kotlin/org/mivoligo/portal_puzzle/MainActivity.kt
@@ -1,6 +1,21 @@
 package org.mivoligo.portal_puzzle
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Aligns the Flutter view vertically with the window.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Disable the Android splash screen fade out animation to avoid
+            // a flicker before the similar frame is drawn in Flutter.
+            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+        }
+
+        super.onCreate(savedInstanceState)
+    }
 }
